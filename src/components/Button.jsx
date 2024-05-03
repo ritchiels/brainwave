97994cc1ac842be1bf7805b5ad5880a3c40f5eb6
Ritchie Simmons <ritchiels.alt@gmail.com>
2024-05-03 01:35:39 -0400

    header complete

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -8,7 +8,7 @@ const Button = ({ className, href, onClick, children, px, white }) => {
     const spanClasses = "relative z-10";
 
     const renderButton = () => (
-        <button className={classes}>
+        <button className={classes} onClick={onClick}>
             <span className={spanClasses}>{children}</span>
             {ButtonSvg(white)}
         </button>
@@ -24,5 +24,3 @@ const Button = ({ className, href, onClick, children, px, white }) => {
 };
 
 export default Button;
-
-//18:35
